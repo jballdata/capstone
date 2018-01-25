92cb128a3add16277898b2ccfa2feab7a267afe4
jballdata <jball.data@gmail.com>
2018-01-24 20:23:45 -0800

    cleared imports

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
-import numpy as np
-import pandas as pd
-import bokeh as bk
-import seaborn as sb
+#import numpy as np
+#import pandas as pd
+#import bokeh as bk
+#import seaborn as sb
 
 app = Flask(__name__)
 
@@ -52,9 +52,9 @@ def index():
 # 		p.line(data.index.values.tolist(),data['Open'].values.tolist(),legend = "line",line_width = 2,line_color = "blue")
 # 		p.line(data.index.values.tolist(),data['Close'].values.tolist(),legend = "line",line_width = 2,line_color = "red")
 		
-#@app.route('/Alabama')
-#def alabama():
-	#return render_template('index.html')
+@app.route('/Alabama')
+def alabama():
+	return render_template('index.html')
 	
 if __name__ == '__main__':
   app.run(port=33507, debug = True)
